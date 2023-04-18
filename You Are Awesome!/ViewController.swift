4552cd960071655c61a67132abc7435c90b38397
Brandon Hobbs <brandon.hobbs1998@gmail.com>
2023-04-18 00:35:48 -0500

    Worked with arrays and shortcuts to be able to cycle through multiple images and messages without stopping or crashing the app.

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,22 +12,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber = 0
+    var messageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "You Are A Legend!",
+                        "You Swifty!",
+                        "You Make Me Smile!",
+                        "I Think You're Fantastic!"]
         
-        print(imageNumber)
+        messageLabel.text = messages[messageNumber]
+        //messageNumber =+ 1 is the same as messageNumber = messageNumber + 1
+        messageNumber = messageNumber + 1
+        
+        if messageNumber == messages.count {
+            messageNumber = 0}
+        
         // let imageName = "image" + String(imageNumber)
         let imageName = "image\(imageNumber)"
         imageView.image = UIImage(named: imageName)
         imageNumber = imageNumber + 1
         if imageNumber == 10 {
-            imageNumber = 0
+            imageNumber = 0}
+            }
         }
-        
         
         //        let awesomeMessage = "You Are Awesome!"
         //        let greatMessage = "You Are Great!"
@@ -43,6 +57,5 @@ class ViewController: UIViewController {
         //            messageLabel.text = awesomeMessage
         //            imageView.image = UIImage(named: "image0")
         //        }
-    }
-}
+
 
